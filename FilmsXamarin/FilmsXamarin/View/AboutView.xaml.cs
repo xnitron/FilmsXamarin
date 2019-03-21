a46f6117504376b23a8dafe593e0faa38d804ff1
xnitronq <xnitronq@gmail.com>
2019-03-21 09:51:32 +0200

    Anima about page

--- FilmsXamarin/FilmsXamarin/View/AboutView.xaml.cs
+++ FilmsXamarin/FilmsXamarin/View/AboutView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using FilmsXamarin.ViewModel;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -11,6 +12,20 @@ namespace FilmsXamarin.View
         {
             InitializeComponent();
             BindingContext = new AboutViewModel();
-        }    
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            await Task.Delay(1000);
+
+            // Start animation
+            await Task.WhenAll(
+                SplashGrid.FadeTo(0, 2000),
+                Logo.ScaleTo(10, 2000)
+                );
+            outerStack.IsVisible = true;
+        }
     }
 }
