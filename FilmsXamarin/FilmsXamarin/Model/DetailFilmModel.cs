455c2a77d563531d1ba5959ba4433ae8deeec18f
xnitronq <xnitronq@gmail.com>
2019-04-12 22:33:03 +0300

    Models

--- FilmsXamarin/FilmsXamarin/Model/DetailFilmModel.cs
+++ FilmsXamarin/FilmsXamarin/Model/DetailFilmModel.cs
@@ -18,6 +18,5 @@ namespace FilmsXamarin.Model
 
         [JsonProperty("runtime")]
         public int Duration { get; set; }
-
     }
 }
--- FilmsXamarin/FilmsXamarin/Model/MenuBarModel.cs
+++ FilmsXamarin/FilmsXamarin/Model/MenuBarModel.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace FilmsXamarin.Model
+﻿namespace FilmsXamarin.Model
 {
     public class MenuBarModel
     {
         public int Id { get; set; }
+
         public string MenuIcon { get; set; }
+
         public string MenuText { get; set; }
     }
 }
