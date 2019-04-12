42586840850b77a967305b2e5c996b5f7fecf188
xnitronq <xnitronq@gmail.com>
2019-04-12 23:39:26 +0300

    Viewss

--- FilmsXamarin/FilmsXamarin/View/AboutView.xaml.cs
+++ FilmsXamarin/FilmsXamarin/View/AboutView.xaml.cs
@@ -3,15 +3,12 @@ using System.Threading.Tasks;
 using Plugin.Compass;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using Plugin.Sensors;
-
 
 namespace FilmsXamarin.View
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AboutView : ContentPage
     {
-
         public AboutView()
         {
             InitializeComponent();
@@ -20,11 +17,13 @@ namespace FilmsXamarin.View
 
             CrossCompass.Current.CompassChanged += (s, e) =>
             {
-                if(e.Heading > 50 & e.Heading < 300)
+                if (e.Heading > 50 & e.Heading < 300)
+                {
                     mainText.FontSize = e.Heading / 10;
+                }
             };
+
             CrossCompass.Current.Start();
-          
         }
 
         protected override async void OnAppearing()
@@ -36,7 +35,8 @@ namespace FilmsXamarin.View
             await Task.WhenAll(
                 SplashGrid.FadeTo(0, 2000),
                 Logo.ScaleTo(10, 2000)
-                );
+            );
+
             outerStack.IsVisible = true;
         }
     }
--- FilmsXamarin/FilmsXamarin/View/DetailFilmView.xaml.cs
+++ FilmsXamarin/FilmsXamarin/View/DetailFilmView.xaml.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FilmsXamarin.ViewModel;
-using FilmsXamarin.Model;
-
-
+﻿using FilmsXamarin.ViewModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
