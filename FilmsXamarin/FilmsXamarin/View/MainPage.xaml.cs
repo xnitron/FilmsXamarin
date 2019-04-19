482d4133224a240bd4a96e22e453c79d9c88a865
xnitronq <xnitronq@gmail.com>
2019-04-20 00:37:23 +0300

    Good move

--- FilmsXamarin/FilmsXamarin/View/MainPage.xaml.cs
+++ FilmsXamarin/FilmsXamarin/View/MainPage.xaml.cs
@@ -6,22 +6,46 @@ namespace FilmsXamarin.View
 {
     public partial class MainPage : MasterDetailPage
     {
+        private const string _password = "123";
+
         public MainPage()
         {
             InitializeComponent();
 
-            Authenticate();
+            AuthenticateWithFingerPrint();
         }
 
-        private async void Authenticate()
+        private async void AuthenticateWithFingerPrint()
         {
             var authResult = await CrossFingerprint.Current.AuthenticateAsync("Put your finger");
 
             if (authResult.Authenticated)
             {
-                Detail = new NavigationPage(new FilmsView());
-                BindingContext = new MenuBarViewModel(this);
+                OnAuthSuccess();
             }
+        }
+
+        private void OnAuthButtonClick(object sender, System.EventArgs e)
+        {
+            if (PasswordField.Text == _password)
+            {
+                OnAuthSuccess();
+            }
+            else
+            {
+                DisplayAlert("Error", "Authentication error", "Ok");
+            }
+        }
+
+        private void OnAuthFingerButtonClick(object sender, System.EventArgs e)
+        {
+            AuthenticateWithFingerPrint();
+        }
+
+        private void OnAuthSuccess()
+        {
+            Detail = new NavigationPage(new FilmsView());
+            BindingContext = new MenuBarViewModel(this);
         }
     }
 }
